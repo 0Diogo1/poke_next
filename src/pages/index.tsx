7de5cc75cc9ef3e803167267580b5c48ff1ace5f
0Diogo1 <diogo.ariau@gmail.com>
2024-02-06 15:01:52 -0400

    feat/homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-
-
-
+import Pokebola from '../../public/images/pokeball.png'
+import Image from "next/image"
 import { Inter } from "next/font/google";
 
 interface Pokemon{
@@ -36,10 +35,16 @@ export async function getStaticProps(){
 
 export default function Home({pokemons}: {pokemons:Pokemon[]}){
   return(
-    <ul>
-      {pokemons.map((pokemon:any) => (
-        <li key={pokemon.id}>{pokemon.name}</li>
-      ))}
-    </ul>
+    <>
+      <div className='flex justify-center items-center mt-10 gap-3'>
+        <h1 className='text-4xl font-bold text-stone-900'>Poke<span className='text-red-700'>Next</span></h1>
+        <Image src={Pokebola} width={60} height={60} alt="Imagem de uma pokebola"/>
+       </div>
+      <div className='flex flex-wrap justify-between items-center max-w-4xl m-auto mt-7'>
+        {pokemons.map((pokemon:any) => (
+          <p key={pokemon.id}>{pokemon.name}</p>
+        ))}
+      </div>
+    </>
   )
 }
